6a408f9845332278d1342bd56b9380aa79a10655
Marmin <marmin.dev@gmail.com>
2024-08-29 23:56:40 +0900

    이니셜 라이즈 쿼리 추가

--- sumsum_init2.sql
+++ sumsum_init2.sql
@@ -3,15 +3,12 @@ CREATE TABLE message (
     user_id bigint not null,
     question varchar(1000) not null,
     reply varchar(1000) not null,
+    emotion varchar(30) not null default '보통',
 	regdate Datetime not null default now()
 );
-
-SELECT * FROM message;
-
 CREATE TABLE user (
 	user_id bigint primary key auto_increment,
 	username varchar(100) not null,
-    connect_ip varchar(20) not null,
+    connect_ip varchar(100) not null,
 	regdate Datetime not null default now()
 );
-
